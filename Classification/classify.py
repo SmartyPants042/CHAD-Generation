53f1370badacd539dc1306b822b410255a4827d8
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-17 10:19:25 +0530

    Cleaning Up the Classifier Model

--- Classification/classify.py
+++ Classification/classify.py
@@ -12,10 +12,6 @@ from nltk.corpus import webtext
 from features_all import features_list
 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
@@ -60,20 +56,11 @@ def extract_features(sentences):
     return features
 
 def fit_model(features, y, test_size=0.20):
-    # vectorizer = CountVectorizer()
-    # X_sents = vectorizer.fit_transform(sentences)
-    # tfidf_transformer = TfidfTransformer()
-    # X_sents = tfidf_transformer.fit_transform(X_sents).toarray()
-    # print("Converted Words to Vectors")
-
-    # mod_sentences = list(zip(X_sents, features))
     X_train, X_test, y_train, y_test = train_test_split(
         features, y, test_size=test_size, random_state=42
     )
     print("Split and Shuffled the Data")
 
-    # print(X_train)
-    # print(X_train)
     classify = LogisticRegression()
     classify.fit(X_train, y_train)
     
@@ -89,7 +76,7 @@ def show_model_results(X_test, y_test):
     print("Accuracy: ", accuracy_score(y_test, y_pred))
     
     plot_confusion_matrix(classify, X_test, y_test)
-    # plt.show()
+    plt.show()
 
 def save_model(name=""):
     pickle.dump(classify, open(f'./Classification/Models/classifier_{name}.model', 'wb'))
@@ -108,13 +95,13 @@ if __name__ == "__main__":
 
     features = extract_features(sentences)
     print("Added features:")
-    for feature in features_list:
-        print("\t", feature.__name__)
+    for i, feature in enumerate(features_list):
+        print(f"\t{i}. {feature.__name__}")
 
     classify, X_test, y_test = fit_model(features, y, test_size=0.2)
     print("Fitted Model")
     
-    show_model_results(X_test, y_test)
+    # show_model_results(X_te`st, y_test)
 
     save_model("LogReg")
     print("Saved Model")
