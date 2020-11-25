60da1527d5549eecb87d9cae9de52dc686fe8e9a
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-25 13:59:22 +0530

    Adding tqdm for better Interactiveness

--- Classification/Visualise/visuals.py
+++ Classification/Visualise/visuals.py
@@ -2,30 +2,26 @@ import json
 import sys
 sys.path.append("./Classification")
 
-from features_all import (
-    alliteration_chain_length, 
-    antonym_pairs,
-    discourse_markers,
-    polarity,
-    absolute_polarity,
-    all_slangs
-)
-    
+from features_all import features_list
+
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-other_sentences = json.load(open("./Corpus/Jokes/reddit_jokes.json"))
-general_sentences = open("./Corpus/General/general.txt").readlines()
+other_sentences = json.load(open("./Corpus/Jokes/reddit_jokes.json"))[:1000]
+general_sentences = open("./Corpus/General/cleaned_general.txt").readlines()[:1000]
 
 def plot_function(function):
+    print(f"Getting general data for {function.__name__} ...")
     general_values = np.array([
-        function(sentence) 
-        for sentence in general_sentences])
+        function(sentence)
+        for sentence in tqdm(general_sentences)])
 
+    print(f"Getting other data for {function.__name__} ...")
     other_values = np.array([
-        function(sentence) 
-        for sentence in other_sentences])
+        function(sentence)
+        for sentence in tqdm(other_sentences)])
     
     _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20, 10))
     ax1.hist(general_values, bins=[i for i in range(10)], edgecolor='black')
@@ -33,5 +29,10 @@ def plot_function(function):
     ax2.hist(other_values, bins=[i for i in range(10)], edgecolor='black')
     plt.show()
 
+    print()
+    print()
+    print()
+
 if __name__ == "__main__":
-    plot_function(all_slangs)
+    for feature in features_list:
+        plot_function(feature)
